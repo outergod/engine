9f5507db2ef42f052fabd5ae22ee563513b24b30
Alexander Kahl <e-user@fsfe.org>
2012-01-31 17:20:45 +0100

    also load all the rope stuff

--- src/engine/repl_init.clj
+++ src/engine/repl_init.clj
@@ -6,4 +6,4 @@
 (set-loggers! :root
               {:pattern "%d %-5p [%t]: %m%n" :level :debug :out *out*})
 
-(require 'engine.core 'engine.client 'engine.client.commands 'engine.server.rope)
+(require 'engine.core 'engine.client 'engine.client.commands 'engine.server.rope 'engine.devel.rope)
