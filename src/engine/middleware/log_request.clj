ea8fdfb56b3e7e10224bd327a369714c902743e7
Alexander Kahl <e-user@fsfe.org>
2012-04-24 20:10:40 +0200

    fixed logging

--- src/engine/middleware/log_request.clj
+++ src/engine/middleware/log_request.clj
@@ -1,7 +1,12 @@
 (ns engine.middleware.log-request
-  (:use [clojure.tools.logging :only [debug]]))
+  (:use [clojure.tools.logging :only (debug)]
+        clj-logging-config.log4j)
+  (:require [clojure.java.io :as io]))
+
+(def access-out (io/file "access.log"))
 
 (defn log [handler]
   (fn [request]
-    (debug (format "Current request is %s" request))
+    (with-logging-config [:root {:pattern "%d %-5p [%t]: %m%n" :level :debug :out access-out}]
+      (debug (format "Current request is %s" request)))
     (handler request)))
