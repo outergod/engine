2e5ac778281c23b4891a81ab344ca9ddf19fa83d
Alexander Kahl <e-user@fsfe.org>
2012-05-05 21:46:40 +0200

    updated dependencies; removed clojurescript dependency (broken)

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
 (defproject engine "0.0.0-SNAPSHOT"
   :description "Engine next generation internet Emacs"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [aleph "0.2.1-alpha2-SNAPSHOT"]
-                 [ring/ring-core "1.0.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [aleph "0.2.1-rc1"]
+                 [ring/ring-core "1.1.0"]
                  [net.cgrand/moustache "1.0.0"]
                  [compojure "0.6.4"]
-                 [hiccup "0.3.7"]
+                 [hiccup "1.0.0"]
                  [cssgen "0.2.5"]
                  [org.clojure/tools.logging "0.2.3"]
                  [clj-logging-config "1.9.6"]]
-  :dev-dependencies [[ring/ring-devel "1.0.0-RC1"]
+  :dev-dependencies [[ring/ring-devel "1.1.0"]
                      [lacij "0.6.0"]
-                     [clojurecheck "2.0.2"]
+                     #_[clojurecheck "2.0.2"] ; broken
                      [incanter "1.3.0"]]
   :resources-path "resources"
   :repl-init engine.repl-init
