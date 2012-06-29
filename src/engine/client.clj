28cd75eaff1e36de0a7ab7a9e57741abe13145bc
Alexander Kahl <e-user@fsfe.org>
2012-06-29 11:37:20 +0200

    added proper css reset styling; improved line height

--- src/engine/client.clj
+++ src/engine/client.clj
@@ -16,7 +16,7 @@
                [:meta {:charset "utf-8"}]
                [:meta {:http-equiv "X-UA-Compatible" :content "chrome=1"}]
                [:link {:rel "shortcut icon" :href "client/gears-32x32.png"}]
-               (apply include-css "client/main.css" "/client/jquery-ui.css" "/client/jquery.pnotify.css" splash/cssfiles)
+               (apply include-css "client/html5reset.css" "client/main.css" "/client/jquery-ui.css" "/client/jquery.pnotify.css" splash/cssfiles)
                [:script {:data-main "client/main" :src "client/require-jquery.js"}]]
               [:body
                [:div {:id "content"} [:pre {:id "editor"}]]
@@ -28,11 +28,12 @@
 
 (defcss "/client/main.css"
   (rule "body" :margin 0 :position "absolute" :top 0 :left 0 :right 0 :bottom 0 :overflow "hidden" :font-size "12px"
-        css-display-box (css-box "orient" "vertical"))
+        css-display-box (css-box "orient" "vertical")
+        (rule ".ui-widget" :font-family "Quantico")
+        (rule ".ui-pnotify-text" :font-family "Inconsolata" :font-size "0.8em"))
   (rule "#content" (css-box "flex" 1) css-display-box) (rule "#content > *" (css-box "flex" 1))
   (rule ".hbox" (css-box "flex" 1) css-display-box (css-box "orient" "horizontal")) (rule ".hbox > *" :width "50%")
   (rule ".vbox" (css-box "flex" 1) css-display-box (css-box "orient" "vertical")) (rule ".vbox > *" :height "50%")
   (rule "#meta" :height "20px")
   (rule "#editor" :position "relative" :margin 0)
-  (rule "#minibuffer" :position "relative" :margin 0 :height "100%")
-  (rule ".ui-pnotify-text" :font-family "Inconsolata" :font-size "0.8em"))
+  (rule "#minibuffer" :position "relative" :margin 0 :height "100%"))
