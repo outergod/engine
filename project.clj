d80cc17e39be5c10b3062ebd7aceafd7f1ba70a4
Alexander Kahl <e-user@fsfe.org>
2012-09-15 14:12:07 +0200

    added license information

--- project.clj
+++ project.clj
@@ -1,3 +1,18 @@
+;;;; Engine - project.clj
+;;;; Copyright (C) 2012  Alexander Kahl <e-user@fsfe.org>
+;;;; This file is part of Engine.
+;;;; Engine is free software; you can redistribute it and/or modify it
+;;;; under the terms of the GNU Affero General Public License as
+;;;; published by the Free Software Foundation; either version 3 of the
+;;;; License, or (at your option) any later version.
+;;;;
+;;;; Engine is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 (defproject engine "0.0.0-SNAPSHOT"
   :description "Engine next generation internet Emacs"
   :dependencies [[org.clojure/clojure "1.4.0"]
--- src/engine/repl_init.clj
+++ src/engine/repl_init.clj
@@ -1,3 +1,18 @@
+;;;; Engine - repl_init.clj
+;;;; Copyright (C) 2012  Alexander Kahl <e-user@fsfe.org>
+;;;; This file is part of Engine.
+;;;; Engine is free software; you can redistribute it and/or modify it
+;;;; under the terms of the GNU Affero General Public License as
+;;;; published by the Free Software Foundation; either version 3 of the
+;;;; License, or (at your option) any later version.
+;;;;
+;;;; Engine is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 (ns engine.repl-init
   (:use clojure.tools.logging
         clj-logging-config.log4j))
