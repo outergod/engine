198bf66cc96838e5854077eb7d1a104373f86422
Alexander Kahl <e-user@fsfe.org>
2012-09-30 17:18:00 +0200

    update to lein2

--- project.clj
+++ project.clj
@@ -15,6 +15,9 @@
 ;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 (defproject engine "0.0.0-SNAPSHOT"
   :description "Engine next generation internet Emacs"
+  :license {:name "GNU Affero General Public License v3 (or later)"
+            :url "http://www.gnu.org/licenses/agpl-3.0.html"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [aleph "0.2.1-rc1"]
                  [ring/ring-core "1.1.0"]
@@ -24,14 +27,17 @@
                  [cssgen "0.2.5"]
                  [org.clojure/tools.logging "0.2.3"]
                  [clj-logging-config "1.9.6"]]
-  :dev-dependencies [[ring/ring-devel "1.1.0"]
-                     [lacij "0.6.0"]
-                     #_[clojurecheck "2.0.2"] ; broken
-                     [incanter "1.3.0"]
-                     [org.clojure/tools.trace "0.7.3"]]
-  :plugins [[lein-swank "1.4.4"]]
+  :profiles {:dev
+             {:dependencies
+              [[org.clojure/tools.nrepl "0.2.0-beta9"]
+               [ring/ring-devel "1.1.0"]
+               [lacij "0.6.0"]
+               #_[clojurecheck "2.0.2"] ; broken
+               [incanter "1.3.0"]
+               [org.clojure/tools.trace "0.7.3"]
+               [midje "1.4.0"]]}}
+  :plugins [[lein-swank "1.4.4"]
+            [lein-midje "1.0.10"]]
   :resources-path "resources"
-  :repl-init engine.repl-init
-  :license {:name "GNU Affero General Public License v3 (or later)"
-            :url "http://www.gnu.org/licenses/agpl-3.0.html"
-            :distribution :repo})
+  :repl-options { :init-ns engine.repl-init }
+  :min-lein-version "2.0.0")
